74c58cf2a878322ecfcad24c09a5de10f5cdfc02
Ellie-Aghajani <z.aqajani@gmail.com>
2023-02-08 01:27:59 -0800

    filtering in mongoose

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Node.js Course',
+        name: 'Angular Course',
         auther: 'Ellie',
-        tags:['node', 'backend'],
+        tags:['angular', 'frontend'],
         isPublished: true
     });
     
@@ -25,3 +25,13 @@ async function createCourse() {
 
 }
 createCourse();
+
+async function getCourses() {
+    const courses = await Course
+        .find({auther:'Ellie', isPublished:true})
+        .limit(10)
+        .sort({name: 1})
+        .select({name: 1, tags: 1});
+    console.log(courses);
+}
+getCourses();
